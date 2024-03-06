4068f5df414ca0d5aa8e260cc0aff1cf13358630
Sion Kang <siontama@gmail.com>
2024-03-06 18:47:12 +0900

    Update test_config.py

--- tests/manager/test_config.py
+++ tests/manager/test_config.py
@@ -23,7 +23,7 @@ def test_shared_config_flatten():
         },
     )
     pprint(data)
-    assert len(data) == 5
+    assert len(data) == 6
     assert data["abc/def"] == "https://example.com"
     assert data["abc/def/normal"] == "okay"
     assert data["abc/def/aa%3Abb%2Fcc/f1"] == "hello"
